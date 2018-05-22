5c5dc6e2632175ffd7ec54680f45c14b33740c56
bethurban <beth.urban@gmail.com>
2018-05-22 19:30:34 -0400

    Added has_many relationships to User, has_secure_password, required username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :places_wished
+  has_many :places_been
+  has_secure_password
+  validates :username, :presence => true
+  validates :email, :presence => true
+
 
 end
