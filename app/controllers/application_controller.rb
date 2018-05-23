b5c00ce3d277c17408a09cfbe6970ad8d3aa1546
bethurban <beth.urban@gmail.com>
2018-05-22 21:02:49 -0400

    Filled in get '/login' and post '/login' routes. Created login form in login.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"])
+    if @user && @user.authenticate(params["password"])
+      session[:user_id] = @user.id
+      redirect "/destinations"
+    else
+      redirect "/login"
+    end
   end
 
   get '/destinations' do
